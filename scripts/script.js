7edd5a8541e213778b174d334121de56ab7d0738
Szymon Oleszek <szymqwerty@gmail.com>
2018-07-10 10:01:35 +0200

    added filter option

---
+++ scripts/script.js
@@ -0,0 +1,21 @@
+const filterNames = () => {
+    let filterValue = document.getElementById('filter-input').value.toUpperCase()
+
+    let ul = document.getElementById('names')
+
+    let li = ul.querySelectorAll('li.collection-item')
+
+    for(let i = 0; i < li.length; i++){
+       
+        let a = li[i].getElementsByTagName('a')[0]
+      if(a.innerHTML.toUpperCase().indexOf(filterValue) !== (-1)){
+        li[i].style.display=''
+      }else {
+        li[i].style.display='none'
+      }
+    }
+}
+
+let filterInput = document.getElementById('filter-input')
+filterInput.addEventListener('keyup', filterNames)
+
